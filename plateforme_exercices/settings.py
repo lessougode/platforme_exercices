736f772fa86c22d590250708c108a4c416d46879
lessougode <lessougode1987@gmail.com>
2026-08-05 11:37:49 +0000

    Ajout DEFAULT_AUTO_FIELD dans settings.py pour corriger les warnings

--- plateforme_exercices/settings.py
+++ plateforme_exercices/settings.py
@@ -19,13 +19,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-y&#+r&br1^@@9i=p!w5+=69m!_hkxkjp!1dx+!3d4t$x+(cj8s'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
-ALLOWED_HOSTS = [' PlateformeExercices.pythonanywhere.com']
+ALLOWED_HOSTS = ['plateformesexercices.pythonanywhere.com']
 
+# Dans plateforme_exercices/settings.py
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # Application definition
 
